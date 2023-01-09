88e731bb46b2ec33b16c3e0d8561adcfdcf8253a
Tomas Hornak <thomas.hornak@gmail.com>
2023-01-09 12:30:09 +0100

    feat: esling + vscode debug setting

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    // "no-unused-vars": ["warn", { varsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-unused-vars": ["warn", { varsIgnorePattern: "^_" }],
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
